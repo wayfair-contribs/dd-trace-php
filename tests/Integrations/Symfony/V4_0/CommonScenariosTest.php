6f407a9d04b7ea4e491362a310fac2cd80035dcd
Luca Abbati <luca.abbati@datadoghq.com>
2019-11-13 13:53:12 +0100

    Migrate Symfony 4.x to Sandboxed api

--- tests/Integrations/Symfony/V4_0/CommonScenariosTest.php
+++ tests/Integrations/Symfony/V4_0/CommonScenariosTest.php
@@ -66,8 +66,13 @@ class CommonScenariosTest extends WebFrameworkTestCase
                                     SpanAssertion::exists('symfony.kernel.response'),
                                     SpanAssertion::exists('symfony.kernel.finish_request'),
                                 ]),
+                            SpanAssertion::exists('symfony.kernel.terminate')
+                                ->skipIf(!static::IS_SANDBOX),
                         ]),
-                    SpanAssertion::exists('symfony.kernel.terminate'),
+                    // This should be fixed for legacy api as this should be a child
+                    // of the root span while here it is a lone trace
+                    SpanAssertion::exists('symfony.kernel.terminate')
+                        ->skipIf(static::IS_SANDBOX),
                 ],
                 'A simple GET request with a view' => [
                     SpanAssertion::build(
@@ -102,10 +107,13 @@ class CommonScenariosTest extends WebFrameworkTestCase
                                     SpanAssertion::exists('symfony.kernel.response'),
                                     SpanAssertion::exists('symfony.kernel.finish_request'),
                                 ]),
+                            SpanAssertion::exists('symfony.kernel.terminate')
+                                ->skipIf(!static::IS_SANDBOX),
                         ]),
-                    // This should be fixed as this should be a child
+                    // This should be fixed for legacy api as this should be a child
                     // of the root span while here it is a lone trace
-                    SpanAssertion::exists('symfony.kernel.terminate'),
+                    SpanAssertion::exists('symfony.kernel.terminate')
+                        ->skipIf(static::IS_SANDBOX),
                 ],
                 'A GET request with an exception' => [
                     SpanAssertion::build(
@@ -140,10 +148,13 @@ class CommonScenariosTest extends WebFrameworkTestCase
                                             SpanAssertion::exists('symfony.kernel.finish_request'),
                                         ]),
                                 ]),
+                            SpanAssertion::exists('symfony.kernel.terminate')
+                                ->skipIf(!static::IS_SANDBOX),
                         ]),
-                    // This should be fixed as this should be a child
+                    // This should be fixed for legacy api as this should be a child
                     // of the root span while here it is a lone trace
-                    SpanAssertion::exists('symfony.kernel.terminate'),
+                    SpanAssertion::exists('symfony.kernel.terminate')
+                        ->skipIf(static::IS_SANDBOX),
                 ],
             ]
         );
